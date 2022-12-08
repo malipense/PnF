c51bfc99620fae2740f5daed48f6ed2bf9c8b6da
Cuco <eduardo.malipense@gmail.com>
2022-12-08 15:32:26 -0300

    initial

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace P_n_F
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadPort readPort1 = new ReadPort();
+            ReadPort readPort2 = new ReadPort();
+
+            Thread thread = new Thread(readPort1.Listen);
+            Thread thread2 = new Thread(readPort2.Listen);
+
+            thread.Start(25000);
+            thread2.Start(25001);
+        }
+    }
+}
---
+++ ReadPort/ReadPort.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System;
+
+namespace P_n_F
+{
+    public class ReadPort
+    {
+        private const int MAXCONNECTIONS = 10;
+        private const int BUFFERSIZE = 2048;
+        public void Listen(object port)
+        {
+            IPAddress ipAddress = IPAddress.Loopback;
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, (int)port);
+
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            socket.Bind(localEndPoint);
+            socket.Listen(MAXCONNECTIONS);
+
+            while(true)
+            {
+                Socket handler = socket.Accept();
+                byte[] buffer = new byte[BUFFERSIZE];
+                int bytesReceived = handler.Receive(buffer, SocketFlags.None);
+
+                var data = Encoding.UTF8.GetString(buffer, 0, bytesReceived);
+                System.Console.WriteLine($"{handler.RemoteEndPoint} - sent the message: {data}");
+
+                Dispose(ref handler, ref buffer, ref bytesReceived);
+            }
+        }
+
+        private void Dispose(ref Socket handler, ref byte[]buffer, ref int bytesReceived)
+        {
+            handler.Close();
+            buffer = Array.Empty<byte>();
+            bytesReceived = 0;
+        }
+    }
+}
